d42fa0c455ea5827e42661af4110a208dcd9d388
Shivansh Pathak <ansh02shiv@gmail.com>
2024-11-19 01:45:23 +0530

    Update layout.tsx for google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YD8VJYGM1X"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-YD8VJYGM1X');
+</script>
       </head>
       <body className={inter.className}>{children}   
       </body>
